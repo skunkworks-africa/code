3957efae10c5bee19df2edd0ccdb32ea03a921bb
Andy <159312225+Mw4mba@users.noreply.github.com>
2024-05-20 23:55:22 +0200

    Update homesearch.js

--- homesearch.js
+++ homesearch.js
@@ -4,7 +4,8 @@ hs=()=>{
 if (sc.value!=null){
     localStorage.setItem('hq1',sc.value)
 };    
-open("./searchsk1.html")
+window.open("./searchsk1.html")
+  
 }
 sc.addEventListener('keypress', function (e) {
     if (e.key === 'Enter') {
